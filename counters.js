aaba4dd2aa14bfa855d9fcf39fef9c92ed5a0140
Dave Winer <dave.winer@gmail.com>
2020-02-25 10:18:46 -0500

    v0.5.7

--- counters.js
+++ counters.js
@@ -1,6 +1,7 @@
-var myVersion = "0.5.2", myProductName = "counters";
+var myVersion = "0.5.7", myProductName = "counters";
 
 const fs = require ("fs");
+const request = require ("request");
 const davehttp = require ("davehttp");
 const s3 = require ("daves3");
 const utils = require ("daveutils");
@@ -8,9 +9,11 @@ const utils = require ("daveutils");
 var config = {
 	port: 1962,
 	flLogToConsole: true,
+	flAllowAccessFromAnywhere: true, //12/17/19 by DW
 	s3Path: "/static.scripting.com/counters/scripting/",
 	fnameToday: "today.json",
-	fnameStats: "stats.json"
+	fnameStats: "stats.json",
+	urlServerHomePageSource: "http://scripting.com/code/counters/index.html"
 	};
 var stats = { 
 	ctHits: 0,
@@ -54,66 +57,104 @@ function saveStats () {
 			});
 		});
 	}
-function count (group, referer, url, callback) {
-	var flnotfound;
-	//referrers
-		if (referer.length > 0) {
-			flnotfound = true;
-			stats.referrers.forEach (function (item) {
-				if (item.url == referer) {
-					item.ct++;
-					flnotfound = false;
+function derefUrl (urlOrig, callback) {
+	var theRequest = {
+		method: "HEAD", 
+		url: urlOrig, 
+		followAllRedirects: true,
+		maxRedirects: 5,
+		headers: {
+			"User-Agent": myProductName + " v" + myVersion
+			}
+		};
+	request (theRequest, function (err, response) {
+		if (err) {
+			callback (err, urlOrig);
+			}
+		else {
+			var urlDeref = response.request.href;
+			callback (undefined, urlDeref);
+			}
+		});
+	}
+function securityCheck (url) { //2/25/20 by DW
+	var params = utils.stringNthField (url, "?", 2);
+	var splits = params.split ("&");
+	var flproblem = false;
+	splits.forEach (function (param) {
+		if (utils.beginsWith (param, "oauth_token")) {
+			flproblem = true;
+			}
+		if (utils.beginsWith (param, "oauth_token_secret")) {
+			flproblem = true;
+			}
+		});
+	return (!flproblem);
+	}
+function count (group, referOrig, url, callback) {
+	if (securityCheck (referOrig) && securityCheck (url)) {
+		derefUrl (referOrig, function (err, referer) {
+			var flnotfound;
+			//referrers
+				if (referer.length > 0) {
+					flnotfound = true;
+					stats.referrers.forEach (function (item) {
+						if (item.url == referer) {
+							item.ct++;
+							flnotfound = false;
+							}
+						});
+					if (flnotfound) {
+						stats.referrers.push ({
+							ct: 1,
+							url: referer
+							});
+						}
+					stats.referrers.sort (function (a, b) {
+						if (b.ct < a.ct) {
+							return (-1);
+							}
+						else {
+							if (a.ct < b.ct) {
+								return (1)
+								}
+							else {
+								return (0);
+								}
+							}
+						});
 					}
-				});
-			if (flnotfound) {
-				stats.referrers.push ({
-					ct: 1,
-					url: referer
+			//pages
+				flnotfound = true;
+				stats.pages.forEach (function (item) {
+					if (item.url == url) {
+						item.ct++;
+						flnotfound = false;
+						}
 					});
-				}
-			stats.referrers.sort (function (a, b) {
-				if (b.ct < a.ct) {
-					return (-1);
+				if (flnotfound) {
+					stats.pages.push ({
+						ct: 1,
+						url: url
+						});
 					}
-				else {
-					if (a.ct < b.ct) {
-						return (1)
+				stats.pages.sort (function (a, b) {
+					if (b.ct < a.ct) {
+						return (-1);
 						}
 					else {
-						return (0);
+						if (a.ct < b.ct) {
+							return (1)
+							}
+						else {
+							return (0);
+							}
 						}
-					}
-				});
-			}
-	//pages
-		flnotfound = true;
-		stats.pages.forEach (function (item) {
-			if (item.url == url) {
-				item.ct++;
-				flnotfound = false;
-				}
+					});
+			statsChanged ();
+			callback (undefined, "We got your ping on " + new Date ().toLocaleString ());
 			});
-		if (flnotfound) {
-			stats.pages.push ({
-				ct: 1,
-				url: url
-				});
-			}
-		stats.pages.sort (function (a, b) {
-			if (b.ct < a.ct) {
-				return (-1);
-				}
-			else {
-				if (a.ct < b.ct) {
-					return (1)
-					}
-				else {
-					return (0);
-					}
-				}
-			});
-	statsChanged ();
-	callback (undefined, "We got your ping on " + new Date ().toLocaleString ());
+		}
 	}
 function readStats (callback) {
 	utils.sureFilePath (config.fnameStats, function () {
@@ -167,6 +208,9 @@ readStats (function () {
 		stats.ctHitsToday++;
 		stats.whenLastHit = new Date ();
 		statsChanged ();
+		function returnHtml (htmltext) {
+			theRequest.httpReturn (200, "text/html", htmltext);
+			}
 		function returnPlainText (s) {
 			theRequest.httpReturn (200, "text/plain", s.toString ());
 			}
@@ -187,7 +231,21 @@ readStats (function () {
 				returnData (jstruct);
 				}
 			}
+		function returnServerHomePage () {
+			request (config.urlServerHomePageSource, function (error, response, templatetext) {
+				if (!error && response.statusCode == 200) {
+					var pagetable = {
+						version: myVersion
+						};
+					var pagetext = utils.multipleReplaceAll (templatetext, pagetable, false, "[%", "%]");
+					returnHtml (pagetext);
+					}
+				});
+			}
 		switch (theRequest.lowerpath) {
+			case "/":
+				returnServerHomePage ();
+				return;
 			case "/version":
 				returnPlainText (myVersion);
 				return;
